d61daf38bfd30fe4f3675844e2bb29eff88c47d4
Sergi Ortiz Gómez <drualcman@msn.com>
2023-05-21 10:26:21 +0800

    change properties to avoid i required uind all default sttings

--- BlazorBasics.Captcha/Entities/CaptchaProperties.cs
+++ BlazorBasics.Captcha/Entities/CaptchaProperties.cs
@@ -1,3 +1,3 @@
 ﻿namespace BlazorBasics.Captcha.Entities;
 
-public record struct CaptchaProperties(CaptchaType Type, string Description, string Placeholder, ButtonType Button = ButtonType.Submit, string ErrorMessage = "Not Match!");
+public record struct CaptchaProperties(CaptchaType Type = CaptchaType.Numeric, string Description = "", string Placeholder = "", ButtonType Button = ButtonType.Submit, string ErrorMessage = "Not Match!");
